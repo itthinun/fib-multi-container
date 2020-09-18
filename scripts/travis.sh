f42e819797b528aaa1917f1fb706a9409684a3c4
bnDev <bnDev@dx.io>
2020-09-18 13:12:16 +0700

    experimental1

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -27,4 +27,6 @@ EOF
 key=$(gpg --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
 pass init $key
 
+ls -l /usr/local/bin/docker-credential-pass
+echo $PATH
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
