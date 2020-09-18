27df005487a9b09a2072b80b7b5358d0dc5d6c50
bnDev <bnDev@dx.io>
2020-09-18 12:02:06 +0700

    x3

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,7 +7,8 @@ sudo apt -y install docker-ce pass
 echo 'DOCKER_OPTS="--experimental"' | sudo tee /etc/default/docker
 sudo service docker restart
 
-curl -fsSlL https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz | sudo tar xzf - -C /usr/local/bin
+curl -fsSlL https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz
+sudo mv docker-credential-pass /usr/local/bin/
 mkdir -p /home/travis/.docker
 echo '{ "credsStore": "pass" }' | tee /home/travis/.docker/config.json
 gpg --batch --gen-key <<-EOF
@@ -25,5 +26,4 @@ EOF
 key=$(gpg --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
 pass init $key
 
-which docker-credential-pass
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
